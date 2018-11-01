28c7e9db28d48e3bbad5459ec6708a4601257863
dhalsey <dhalsey@ucsc.edu>
2018-11-01 11:24:27 -0700

    initial commit

---
+++ craft_planner.py
@@ -0,0 +1,259 @@
+import json
+from collections import namedtuple, defaultdict, OrderedDict
+from timeit import default_timer as time
+from heapq import heappop, heappush
+
+Recipe = namedtuple('Recipe', ['name', 'check', 'effect', 'cost'])
+
+make_tools = {'craft bench': 'bench', 'craft furnace at bench': 'furnace', 'craft iron_axe at bench': 'iron_axe', 'craft iron_pickaxe at bench': 'iron_pickaxe', 'craft stone_axe at bench': 'stone_axe', 'craft stone_pickaxe at bench': 'stone_pickaxe', 'craft wooden_axe at bench': 'wooden_axe', 'craft wooden_pickaxe at bench': 'wooden_pickaxe'}
+
+class State(OrderedDict):
+    """ This class is a thin wrapper around an OrderedDict, which is simply a dictionary which keeps the order in
+        which elements are added (for consistent key-value pair comparisons). Here, we have provided functionality
+        for hashing, should you need to use a state as a key in another dictionary, e.g. distance[state] = 5. By
+        default, dictionaries are not hashable. Additionally, when the state is converted to a string, it removes
+        all items with quantity 0.
+
+        Use of this state representation is optional, should you prefer another.
+    """
+
+    def __key(self):
+        return tuple(self.items())
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __lt__(self, other):
+        return self.__key() < other.__key()
+
+    def copy(self):
+        new_state = State()
+        new_state.update(self)
+        return new_state
+
+    def __str__(self):
+        return str(dict(item for item in self.items() if item[1] > 0))
+
+
+def make_checker(rule):
+    # Implement a function that returns a function to determine whether a state meets a
+    # rule's requirements. This code runs once, when the rules are constructed before
+    # the search is attempted.
+
+    hasRequirements = 'Requires' in rule.keys()
+    ConsumesItems = 'Consumes' in rule.keys()
+
+    def check(state):
+        # This code is called by graph(state) and runs millions of times.
+        # Tip: Do something with rule['Consumes'] and rule['Requires'].
+        if ConsumesItems:
+            for item in rule['Consumes']:
+                if state[item] < rule['Consumes'][item]:
+                    return False
+        if hasRequirements:
+            for item in rule['Requires']:
+                if state[item] < 1:
+                    return False
+        return True
+
+    return check
+
+
+def make_effector(rule):
+    # Implement a function that returns a function which transitions from state to
+    # new_state given the rule. This code runs once, when the rules are constructed
+    # before the search is attempted.
+
+    ConsumesItems = 'Consumes' in rule.keys() 
+
+    def effect(state):
+        # This code is called by graph(state) and runs millions of times
+        # Tip: Do something with rule['Produces'] and rule['Consumes'].
+        next_state = state.copy()
+        for item in rule['Produces']:
+            next_state[item] = state[item] + rule['Produces'][item]
+
+        if ConsumesItems:
+            for item in rule['Consumes']:
+                next_state[item] = state[item] - rule['Consumes'][item]
+
+        next_state['Time'] += rule['Time']
+
+        return next_state
+
+    return effect
+
+
+def make_goal_checker(goal):
+    # Implement a function that returns a function which checks if the state has
+    # met the goal criteria. This code runs once, before the search is attempted.
+
+    def is_goal(state):
+        # This code is used in the search process and may be called millions of times.
+        for item in goal:
+            if state[item] < goal[item]:
+                return False
+        return True
+
+    return is_goal
+
+
+def graph(state):
+    # Iterates through all recipes/rules, checking which are valid in the given state.
+    # If a rule is valid, it returns the rule's name, the resulting state after application
+    # to the given state, and the cost for the rule.
+    for r in all_recipes:
+        if r.check(state):
+            yield (r.name, r.effect(state), r.cost)
+
+
+def heuristic(state, action, is_goal, tools_in_goal):
+    # Implement your heuristic here!
+
+    if not action:
+        return 0;
+
+    if is_goal(state):
+        return 0
+
+    if action.name in make_tools.keys() and tools_in_goal is False:
+        if state[make_tools[action.name]] > 1:
+            return 2000
+
+    return 0
+
+def search(graph, state, is_goal, limit, heuristic):
+
+    start_time = time()
+
+    # Implement your search here! Use your heuristic here!
+    # When you find a path to the goal return a list of tuples [(state, action)]
+    # representing the path. Each element (tuple) of the list represents a state
+    # in the path and the action that took you to this state
+    
+    #A* search of action space
+
+    # initialize empty queue
+    queue = []
+
+    # initialize empty dictionary of backpointers
+    backpointers = {}
+
+    # set of visited states
+    visited = set()
+
+    # add source to queue
+    heappush(queue, (0, 0, state, None))
+
+    #mark source as visited
+    test_state = state.copy()
+    test_state['Time'] = 0
+    visited.add(test_state)
+
+    while time() - start_time < limit:
+        # expand node if queue is non-empty
+        if queue:
+            # get current node with cost
+            current_priority, current_cost, current_state, parent_action = heappop(queue)
+
+            print(current_state)
+
+            # construct and return path when goal is reached
+            if is_goal(current_state):
+                # initialize path (state, action)
+                path = []
+
+                # initialize temp state and action
+                temp_state = current_state
+                temp_action = parent_action
+
+                # append goal action state
+                path.insert(0, (temp_state, temp_action))
+
+                # construct path from backpointers
+                while backpointers[(temp_state, temp_action)][1] is not None:
+                    temp_state, temp_action = backpointers[(temp_state, temp_action)]
+                    path.insert(0, (temp_state, temp_action.name))
+
+                print(time(), " seconds")
+                print(len(path), " steps")
+
+                return path
+
+            # expand current state with all valid actions
+            for r in all_recipes:
+                if r.check(current_state):
+    
+                    # get next state given action r
+                    new_state = r.effect(current_state)
+                    test_state = new_state.copy()
+                    test_state['Time'] = 0
+                    if test_state not in visited:
+                        # get cost of action r
+                        cost = current_cost + r.cost
+
+                        # get priority with heuristic
+                        priority = cost + heuristic(new_state, r, is_goal, False)
+
+                        # push next state with parent action
+                        heappush(queue, (priority, cost, new_state, r))
+
+                        # set backpointer
+                        backpointers[(new_state, r)] = (current_state, parent_action)
+
+                        # set test_state as visited
+                        visited.add(test_state) 
+        pass
+
+    # Failed to find a path
+    print(time() - start_time, 'seconds.')
+    print("Failed to find a path from", state, 'within time limit.')
+    return None
+
+if __name__ == '__main__':
+    with open('Crafting.json') as f:
+        Crafting = json.load(f)
+
+    # # List of items that can be in your inventory:
+    # print('All items:', Crafting['Items'])
+    #
+    # # List of items in your initial inventory with amounts:
+    # print('Initial inventory:', Crafting['Initial'])
+    #
+    # # List of items needed to be in your inventory at the end of the plan:
+    # print('Goal:',Crafting['Goal'])
+    #
+    # # Dict of crafting recipes (each is a dict):
+    # print('Example recipe:','craft stone_pickaxe at bench ->',Crafting['Recipes']['craft stone_pickaxe at bench'])
+
+    # Build rules
+    all_recipes = []
+    for name, rule in Crafting['Recipes'].items():
+        checker = make_checker(rule)
+        effector = make_effector(rule)
+        recipe = Recipe(name, checker, effector, rule['Time'])
+        all_recipes.append(recipe)
+
+    # Create a function which checks for the goal
+    is_goal = make_goal_checker(Crafting['Goal'])
+
+    # Initialize first state from initial inventory
+    state = State({key: 0 for key in Crafting['Items']})
+    state['Time'] = 0
+    state.update(Crafting['Initial'])
+
+    # test rules
+    #for r in all_recipes:
+    #    print(r.name)
+    #    print(r.check(state))
+    #    if r.check(state):
+    #        print(r.effect(state))
+
+    # Search for a solution
+    resulting_plan = search(graph, state, is_goal, 600, heuristic)
+
+    if resulting_plan:
+        # Print resulting plan
+        for state, action in resulting_plan:
+            print('\t',state)
+            print(action)
